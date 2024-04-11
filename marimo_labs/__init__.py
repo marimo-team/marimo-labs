d5f660900189f236b2cced256012e4a78ed33b4c
Akshay Agrawal <akshayka@cs.stanford.edu>
2024-04-11 11:37:17 -0700

    run black

--- marimo_labs/__init__.py
+++ marimo_labs/__init__.py
@@ -6,7 +6,9 @@ import marimo_labs.huggingface as huggingface
 try:
     __version__ = importlib.metadata.version(__name__)
 except importlib.metadata.PackageNotFoundError as e:
-    warnings.warn(f"Could not determine version of {__name__}\n{e!r}", stacklevel=2)
+    warnings.warn(
+        f"Could not determine version of {__name__}\n{e!r}", stacklevel=2
+    )
     __version__ = "unknown"
 
 __all__ = ["huggingface"]
